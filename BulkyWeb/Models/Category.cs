3b4dfa0319aec9b2b8d4fad61f5258cef34e37e7
RuchitaGanni <ruchitaganni@gmail.com>
2023-04-19 23:42:18 +0530

    Added create button and made changes related to category controller

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -8,8 +9,11 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
     [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
-
+        [DisplayName("Display Order")]
+        [Range(1, 30,ErrorMessage ="Display order must be between 1-30")]
         public int DisplayOrder { get; set; }
     }
 }
